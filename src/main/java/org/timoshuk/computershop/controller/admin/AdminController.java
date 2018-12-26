788a63e08b85f99c4d8727b4a383bccba7710acb
pavel <paveltimosh@gmail.com>
2018-12-26 14:02:47 +0300

    Add admin controllers for management users and orders

--- src/main/java/org/timoshuk/computershop/controller/admin/AdminController.java
+++ src/main/java/org/timoshuk/computershop/controller/admin/AdminController.java
@@ -1,4 +1,45 @@
 package org.timoshuk.computershop.controller.admin;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.timoshuk.computershop.DTO.UserDTO;
+import org.timoshuk.computershop.DTO.view.UserDTOview;
+import org.timoshuk.computershop.exception.EntityNotFoundException;
+import org.timoshuk.computershop.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admins")
 public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @JsonView(UserDTOview.ShowUser.class)
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<UserDTO> showAllUsers(){
+        List<UserDTO> users = userService.findAll();
+        return users;
+    }
+
+    @JsonView(UserDTOview.ShowUser.class)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public UserDTO showUserById(@PathVariable Long id){
+        UserDTO user = userService.findById(id);
+        if (user == null){
+            throw new EntityNotFoundException("User not found.");
+        }
+        return user;
+    }
+
+
+
+
+
+
 }
--- src/main/java/org/timoshuk/computershop/controller/admin/AdminOrderController.java
+++ src/main/java/org/timoshuk/computershop/controller/admin/AdminOrderController.java
@@ -1,4 +1,56 @@
 package org.timoshuk.computershop.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.timoshuk.computershop.entity.order.Order;
+import org.timoshuk.computershop.service.OrderService;
+import org.timoshuk.computershop.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admins")
 public class AdminOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "users/orders", method = RequestMethod.GET)
+    public List<Order> showAllOrders(){
+        List<Order> orderList = orderService.findAll();
+        return orderList;
+    }
+
+    @RequestMapping(value = "users/{id}/orders", method = RequestMethod.GET)
+    public List<Order> showAllOrdersUserById(@PathVariable Long id){
+        List<Order> orderList = orderService.findAllByUserId(id);
+        return orderList;
+    }
+
+    @RequestMapping(value = "/orders/{idOrder}", method = RequestMethod.GET)
+    public Order showOrderById(@PathVariable("idOrder") Long idOrder){
+        Order order = orderService.findById(idOrder);
+        return order;
+    }
+
+    @RequestMapping(value = "/orders/status/{orderStatus}", method = RequestMethod.GET)
+    public List<Order> showOrdersByOrderStatus(@PathVariable String orderStatus){
+        List<Order> orderList = orderService.findAllByOrderStatus(orderStatus);
+        return orderList;
+    }
+
+    @RequestMapping(value = "/orders/{idOrder}/{orderStatus}", method = RequestMethod.PUT)
+    public Order changeOrderStatus(@PathVariable Long idOrder,
+                                   @PathVariable String orderStatus){
+        Order order = orderService.findById(idOrder);
+        orderService.changeOrder(order,orderStatus);
+        orderService.update(order);
+        return order;
+    }
 }
